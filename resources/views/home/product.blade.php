be18a7c366e0efe804df4bbeea4790a22418cbb0
[JohanChate] <[jachate7@misena.edu.co]>
2023-10-04 18:46:28 -0500

    up

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -5,40 +5,7 @@
 
         <h1 class="heading"> nuestros <span>productos</span> </h1>        
         <div class="box-container">
-            @foreach($productos as $producto)
-    <div class="box">
-        <div class="icons">
-            <a href="#" class="fas fa-shopping-cart"></a>
-            <a href="#" class="fas fa-heart"></a>
-            <a href="#" class="fas fa-eye modal-popup" ></a>
-        </div>
-        <div class="image">
-            <img src="{{ asset('storage/' . $producto->image) }}" alt="">
-        </div>
-        <div class="content">
-            <h3>{{ $producto->name}}</h3>
-            <div class="stars">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star-half-alt"></i>
-            </div>
-            {{-- detalle --}}
-            <div class="modal-frame">
-                <div class="modal-body">
-                    <div class="modal-inner">
-                        <button id="close" class="close"><i class="fas fa-times"></i></button>
-                        <img src="{{ asset('storage/' . $producto->image) }}" alt="Image">
-                        <p>{{ $producto->description}}</p>
-                    </div>
-                </div>
-                <div class="modal-overlay"></div>
-            </div>
-            <div class="price">{{ $producto->price }} <span>{{ $producto->price }}</span></div>
-        </div>
-    </div>
-@endforeach
+            
           @foreach($productos as $row)
             <div class="box">
                 <div class="icons">
