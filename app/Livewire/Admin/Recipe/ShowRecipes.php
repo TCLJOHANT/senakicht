b00c3a7be55492854fbd9ae15b1005829cf98d04
johan chate <jachate7@misena.edu.co>
2023-10-18 19:39:34 +0200

    10%

--- app/Livewire/Admin/Recipe/ShowRecipes.php
+++ app/Livewire/Admin/Recipe/ShowRecipes.php
@@ -6,14 +6,34 @@ use Livewire\Component;
 
 class ShowRecipes extends Component
 {
-    public $search;
+    public $search,$recipe,$images,$identificador;
     public $ordenar = "";
     public $direction = "desc";
     protected $paginationTheme = "bootstrap";
+    public $open_edit = false;
+
+    public $rules = [
+        'recipe.name'=> 'required',
+        'recipe.images'=> 'required|image|mimes:png,jpg|max:2048',
+        'recipe.description'=> 'required',
+        'recipe.ingredients' =>'required',
+        'recipe.preparation' =>'required',
+        // 'category' => 'required',
+    ];
     protected $listeners = ['render'=>'render'];
+
+    public function mount(){
+        $this->identificador = rand(); //le asigna un numero al azar o random
+        $this->recipe = new Recipe(); 
+    }
     public function render()
     {   
         $recipes = Recipe::where('name', 'LIKE', '%' . $this->search . '%')->get();
         return view('livewire.admin.recipe.show-recipes',compact('recipes'));
+    }
+
+    public function edit(Recipe $recipe){
+        $this->recipe = $recipe;
+        $this->open_edit = true;
     }
 }
--- resources/views/livewire/admin/recipe/show-recipes.blade.php
+++ resources/views/livewire/admin/recipe/show-recipes.blade.php
@@ -20,14 +20,14 @@
                         </tr>
                     </thead>
                     <tbody >
-                        @foreach ($recipes as $recipe)
+                        @foreach ($recipes as $item)
                             <tr >
-                                <td class="px-6 py-6">{{$recipe->name}}</td>
-                                <td class="px-6 py-6">{{$recipe->description}}</td>
-                                <td class="px-6 py-6">{{$recipe->ingredients}}</td>
-                                <td class="px-6 py-6">{{$recipe->preparation}}</td>
+                                <td class="px-6 py-6">{{$item->name}}</td>
+                                <td class="px-6 py-6">{{$item->description}}</td>
+                                <td class="px-6 py-6">{{$item->ingredients}}</td>
+                                <td class="px-6 py-6">{{$item->preparation}}</td>
                                 <td class="p-0">
-                                    <a class="font-bold text-white py-2 px-4 rounded cursor-pointer bg-green-600 hover:bg-green-500" >  
+                                    <a wire:click="edit({{$item}})" class="font-bold text-white py-2 px-4 rounded cursor-pointer bg-red-600 hover:bg-red-500" >  
                                         <i class="fas fa-edit"></i>
                                     </a> 
                                     <form action="" method="post" style="display: inline;">
@@ -52,5 +52,58 @@
         {{-- {{$users->links()}} --}}
     </div>
  </div>
-</div>
+
+
+
+
+
+
+
+
+    <x-dialog-modal wire:model="open_edit">
+        <x-slot name="title">
+            Crear un nueva Receta
+        </x-slot>
+        <x-slot name="content">
+            <div wire:loading wire:target="images" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Imagen Cargando!</strong>
+                <span class="block sm:inline">Espere un momento hasta que la imagen se procece.</span>
+            </div>
+            <div class="mb-4">
+                <x-label value="Nombre de Receta"></x-label>
+                <x-input type="text" wire:model="recipe.name"></x-input>
+                <x-input-error for='name'></x-input-error>
+            </div>
+            @if ($images)
+                <img class="mb-4 " src="{{$images->temporaryUrl()}}" alt="" width="100px">
+            @endif
+            <div class="mb-4">
+                <x-label value="Imagen de la Receta"></x-label>
+                <input type="file" wire:model="images" id="{{$identificador}}">
+                <x-input-error for='images'></x-input-error>
+            </div>
+            <div class="mb-4">
+                <x-label value="Descripción de la Receta"></x-label>
+                <textarea name="" class="form-control w-full" id="" rows="6" wire:model="description"></textarea>
+                <x-input-error for='description'></x-input-error>
+            </div>
+            <div class="mb-4">
+                <x-label value="Ingredientes de la Receta"></x-label>
+                <textarea name="" class="form-control w-full" id="" rows="6" wire:model="ingredients"></textarea>
+                <x-input-error for='ingredients'></x-input-error>
+            </div>
+            <div class="mb-4">   
+                <x-label value="Preparación de la Receta"></x-label>
+                <textarea name="" class="form-control w-full" id="" rows="6" wire:model="preparation"></textarea>
+                <x-input-error for='preparation'></x-input-error>
+            </div>
+
+        </x-slot>
+        <x-slot name="footer">
+            <x-secondary-button  wire:click="$set('open_edit', false)">Cancelar</x-secondary-button>
+            <x-danger-button class="disabled:opacity-25" wire:loading.attr="disabled" wire:click="save"  wire:target="save,images">Crear Receta</x-danger-button>
+
+
+        </x-slot>
+    </x-dialog-modal>
 </div>
