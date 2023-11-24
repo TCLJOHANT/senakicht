b4d4876d22865a36f20a9a703841a7785deafe7e
cambios de german <gerdbanavides@misena.edu.co>
2023-11-24 09:43:00 -0500

    cambios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('descripcion')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('ubicacion')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
