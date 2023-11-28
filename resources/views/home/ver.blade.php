287a441990681f1f754cc4c6e2a6f2e6eb831c24
L0P3ZS <yoniersebastian23@gmail.com>
2023-11-28 17:56:48 -0500

    datos de envio

--- resources/views/home/ver.blade.php
+++ resources/views/home/ver.blade.php
@@ -3,88 +3,102 @@
 <head>
   <title>Formulario de Recetas</title>
   <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@1.9.6/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
+<!-- Botón para abrir el modal -->
+<button id="openModalBtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Abrir Modal
+</button>
 
-<style>
-    /* Estilos para el formulario */
-body {
-  font-family: Arial, sans-serif;
-  margin: 30px;
-}
+<!-- Modal -->
+<div id="myModal" class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center hidden">
+  <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 
-h1 {
-  text-align: center;
-}
-
-.form-group {
-  margin-bottom: 20px;
-}
-
-label {
-  font-weight: bold;
-}
-
-input[type="text"],
-textarea,
-select {
-  width: 100%;
-  padding: 8px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-
-input[type="submit"] {
-  padding: 10px 20px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-}
-
-input[type="submit"]:hover {
-  background-color: #45a049;
-}
-
-</style>
-
-<h1>Formulario de Recetas</h1>
-
-<form action="/guardar_receta" method="post">
-  <div class="form-group">
-    <label for="nombre_receta">Nombre de la receta:</label><br>
-    <input type="text" id="nombre_receta" name="nombre_receta" required><br><br>
+  <div class="modal-content bg-white w-1/2 rounded shadow-lg p-8">
+    <div class="modal-header flex justify-between border-b-2 border-gray-200 pb-3">
+      <h2 class="text-xl font-semibold">Este es un Modal</h2>
+      <button id="closeBtn" class="text-gray-500 hover:text-gray-700 text-2xl font-bold">&times;</button>
+    </div>
+    <div class="modal-body py-4">
+      <form class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              First Name
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+            <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+              Last Name
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+              Password
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
+            <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-2">
+          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+              City
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque">
+          </div>
+          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+              State
+            </label>
+            <div class="relative">
+              <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                <option>New Mexico</option>
+                <option>Missouri</option>
+                <option>Texas</option>
+              </select>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+              Zip
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210">
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
+</div>
 
-  <div class="form-group">
-    <label for="ingredientes">Ingredientes:</label><br>
-    <textarea id="ingredientes" name="ingredientes" rows="4" cols="50" required></textarea><br><br>
-  </div>
+<!-- JavaScript -->
+<script>
+  const openModalBtn = document.getElementById('openModalBtn');
+  const modal = document.getElementById('myModal');
+  const closeBtn = document.getElementById('closeBtn');
 
-  <div class="form-group">
-    <label for="instrucciones">Instrucciones:</label><br>
-    <textarea id="instrucciones" name="instrucciones" rows="8" cols="50" required></textarea><br><br>
-  </div>
+  openModalBtn.addEventListener('click', function() {
+    modal.classList.remove('hidden');
+  });
 
-  <div class="form-group">
-    <label for="tiempo_preparacion">Tiempo de preparación:</label><br>
-    <input type="text" id="tiempo_preparacion" name="tiempo_preparacion"><br><br>
-  </div>
+  closeBtn.addEventListener('click', function() {
+    modal.classList.add('hidden');
+  });
 
-  <div class="form-group">
-    <label for="dificultad">Dificultad:</label><br>
-    <select id="dificultad" name="dificultad">
-      <option value="facil">Fácil</option>
-      <option value="intermedia">Intermedia</option>
-      <option value="dificil">Difícil</option>
-    </select><br><br>
-  </div>
-
-  <input type="submit" value="Guardar receta">
-</form>
-
+  modal.addEventListener('click', function(event) {
+    if (event.target === modal) {
+      modal.classList.add('hidden');
+    }
+  });
+</script>
 </body>
 </html>
